5c536770583493bc10459b0ed52fef14bffd777d
Steve Tsiopanos <stevetsi@annatech.com>
2018-12-02 17:25:49 -0500

    Autoload cAPI classes from annatech/capi2-libraries

---
+++ services/capi/capi/Capi/CapiMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the annatech/capi2-libraries package.
+ *
+ * (c) Steve Tsiopanos <steve.tsiopanos@annatech.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Capi;
+
+class CapiMiddleware
+{
+
+}
